18144289c6bc6895396e5982c1b1080b6cdc4c73
Ismail <ismailisrailov986@gmail.com>
2026-01-08 02:28:27 +0600

    update project

--- js/home_works.js
+++ js/home_works.js
@@ -19,18 +19,62 @@ gmlBtn.onclick = () => {
 const parendBlock = document.querySelector(".parent_block");
 const childBlock = document.querySelector(".child_block");
 
-let pixel = 0;
+let positionX = 0;
+let positionY = 0;
 
-parendBlock.onclick = () => {
-    const count = () => {
-        pixel++ ;
-        childBlock.style.left = pixel + 'px';
-        console.log(pixel)
-        if(pixel < 450){
-            requestAnimationFrame(count);
-        };
-    };
-    count();
+const offsetWidth = parendBlock.offsetWidth - childBlock.offsetWidth;
+const offsetHeight = parendBlock.offsetHeight - childBlock.offsetHeight;
 
+const moveBlock = () => {
     
-};
+    if (positionX < offsetWidth && positionY === 0){
+        positionX++;
+    childBlock.style.left = positionX + 'px';
+        requestAnimationFrame(moveBlock);   
+        
+    }
+    else if (positionX >= offsetWidth && positionY < offsetHeight) {
+        positionY++;
+        childBlock.style.top = positionY + 'px';
+        requestAnimationFrame(moveBlock);
+    }
+    else if (positionY >= offsetHeight && positionX > 0) {
+        positionX--;
+        childBlock.style.left = positionX + 'px'
+        requestAnimationFrame(moveBlock);
+    }else if (positionX <= 0 && positionY > 0) {
+        positionY--;
+        childBlock.style.top = positionY + 'px'
+        requestAnimationFrame(moveBlock);
+    };
+
+};moveBlock();
+console.log(childBlock);
+
+const time = document.querySelector("#seconds");
+const start = document.querySelector("#start");
+const stop = document.querySelector("#stop");
+const reset = document.querySelector("#reset");
+
+let sec = 0;
+let interval = null;
+
+start.addEventListener("click", () => {
+    if (interval !== null) return;
+    interval = setInterval(() => {
+        sec++;
+        time.innerHTML = sec;
+    }, 1000);
+});
+
+stop.addEventListener("click", () => {
+    clearInterval(interval);
+    interval = null;
+});
+
+reset.addEventListener("click", () => {
+    clearInterval(interval);
+    interval = null;
+    sec = 0;
+    time.innerHTML = sec;
+});
